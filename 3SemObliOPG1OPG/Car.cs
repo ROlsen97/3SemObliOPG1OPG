c901dbd2bfc6ff4c713aaf796f2aedb081852d90
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2023-02-27 16:14:10 +0100

    Add project files.

---
+++ 3SemObliOPG1OPG/Car.cs
@@ -0,0 +1,41 @@
+﻿namespace _3SemObliOPG1OPG
+{
+    using System.Reflection;
+        public class Car
+        {
+            public int Id { get; set; }
+            public string? Model { get; set; }
+            public int Price { get; set; }
+            public string? Licenseplate { get; set; }
+
+            public override string ToString()
+            {
+                return Id + " " + Model + " " + Price + " " + Licenseplate;
+            }
+
+            public void ModelValidering()
+            {
+                if (Model?.Length <= 4)
+                    throw new ArgumentException($"Model: {Model} længden er mindre end 4, tilføj flere tegn");
+            }
+            public void PriceValidering()
+            {
+                if (Price <= 0)
+                    throw new ArgumentOutOfRangeException($"Prisen: {Price} er mindre end eller det samme som nul, den skal være højere");
+            }
+            public void LicensePlateValidering()
+            {
+                if (Licenseplate?.Length <= 2)
+                    throw new ArgumentException($"Nummerpladen : {Licenseplate} har mindre end 2 tegn");
+
+                if (Licenseplate?.Length >= 7)
+                    throw new ArgumentException($"Nummerpladen : {Licenseplate} har flere end 7 tegn");
+            }
+            public void Validate()
+            {
+                ModelValidering();
+                PriceValidering();
+                LicensePlateValidering();
+            }
+        }
+    }
---
+++ 3SemObliOPG1OPGTests/CarTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3SemObliOPG1OPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3SemObliOPG1OPG.Tests
+{
+        [TestClass()]
+        public class CarTests
+        {
+            private Car DenOptimaleBil = new() { Id = 1, Model = "FantasiModel", Licenseplate = "BA3336", Price = 100 };
+            private Car DenFejlerModelBil = new() { Id = 2, Model = "UIO", Licenseplate = "BA3336", Price = 100 };
+            private Car DenFejlerPrisBil = new() { Id = 3, Model = "FantasiModelV2", Licenseplate = "BA3336", Price = -100 };
+            private Car DenFejlerNummerPladeBil = new() { Id = 4, Model = "FantasiModelV3", Licenseplate = "B", Price = 100 };
+
+            [TestMethod()]
+            public void ToStringTest()
+            {
+                string str = DenOptimaleBil.ToString();
+                Assert.AreEqual("1 FantasiModel 100 BA3336", str);
+            }
+
+
+            [TestMethod()]
+            public void ModelValideringTest()
+            {
+                DenOptimaleBil.ModelValidering();
+                Assert.ThrowsException<ArgumentException>(() => DenFejlerModelBil.ModelValidering());
+            }
+
+            [TestMethod()]
+            public void PriceValideringTest()
+            {
+                DenOptimaleBil.PriceValidering();
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => DenFejlerPrisBil.PriceValidering());
+            }
+
+            [TestMethod()]
+            public void LicensePlateValideringTest()
+            {
+                DenOptimaleBil.LicensePlateValidering();
+                Assert.ThrowsException<ArgumentException>(() => DenFejlerNummerPladeBil.LicensePlateValidering());
+            }
+
+            [TestMethod()]
+            public void ValidateTest()
+            {
+                DenOptimaleBil.Validate();
+                Assert.ThrowsException<ArgumentException>(() => DenFejlerModelBil.ModelValidering());
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => DenFejlerPrisBil.PriceValidering());
+                Assert.ThrowsException<ArgumentException>(() => DenFejlerNummerPladeBil.LicensePlateValidering());
+            }
+        }
+}
